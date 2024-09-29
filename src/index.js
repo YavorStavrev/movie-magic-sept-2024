b0248e6b95b15f1305365c232224aa52f2e01387
YavorStavrev <156719018+YavorStavrev@users.noreply.github.com>
2024-09-29 20:35:17 +0300

    Add body parser middleware

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
+app.use(express.urlencoded({extended: false}));
+
 app.use(express.static('public'));
 
 app.use(routes);
