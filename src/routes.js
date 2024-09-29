6bd176af71a37659ed9e4a0b3e0d83be9c391acb
YavorStavrev <156719018+YavorStavrev@users.noreply.github.com>
2024-09-29 20:30:27 +0300

    Add create movie page

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,12 @@ import { Router } from 'express';
 
 import homeController from './controllers/homeController.js';
 
+import movieController from './controllers/movieController.js';
+
 const router = Router();
 
 router.use(homeController);
 
+router.use('/movies', movieController);
 
 export default router;
